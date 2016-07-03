156f4f32a9e4230a87abad09eec7864bcf67ae5f
RayMMond <raymodsyp@gmail.com>
2016-07-03 21:11:38 +0800

    2016-7-3

---
+++ ReadDbfFile/ReadDbfFile/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ReadDbfFile
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSelect = new System.Windows.Forms.Button();
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.btReadFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btSelect
+            // 
+            this.btSelect.Location = new System.Drawing.Point(464, 11);
+            this.btSelect.Name = "btSelect";
+            this.btSelect.Size = new System.Drawing.Size(33, 23);
+            this.btSelect.TabIndex = 0;
+            this.btSelect.Text = "...";
+            this.btSelect.UseVisualStyleBackColor = true;
+            this.btSelect.Click += new System.EventHandler(this.btSelect_Click);
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.Location = new System.Drawing.Point(96, 12);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.Size = new System.Drawing.Size(362, 21);
+            this.tbFilePath.TabIndex = 1;
+            // 
+            // btReadFile
+            // 
+            this.btReadFile.Location = new System.Drawing.Point(517, 12);
+            this.btReadFile.Name = "btReadFile";
+            this.btReadFile.Size = new System.Drawing.Size(75, 23);
+            this.btReadFile.TabIndex = 2;
+            this.btReadFile.Text = "ReadFile";
+            this.btReadFile.UseVisualStyleBackColor = true;
+            this.btReadFile.Click += new System.EventHandler(this.btReadFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "FilePath";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(13, 37);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(989, 591);
+            this.dataGridView.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 640);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btReadFile);
+            this.Controls.Add(this.tbFilePath);
+            this.Controls.Add(this.btSelect);
+            this.Name = "MainForm";
+            this.Text = "ReadDbfFile";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSelect;
+        private System.Windows.Forms.TextBox tbFilePath;
+        private System.Windows.Forms.Button btReadFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
